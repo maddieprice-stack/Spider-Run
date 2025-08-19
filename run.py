c9877a422f5ce7d9a50383d482103437977b5ef8
maddieprice-stack <maddieprice@stack.com>
2025-08-18 23:14:43 -0500

    Level 1 splash -> gameplay: set currentState to gameplay so board renders (fix black/blue screen)

--- run.py
+++ run.py
@@ -2570,6 +2570,7 @@ GAME_HTML = """
             
             setTimeout(() => {
                 level1State = 'gameplay';
+                currentState = 'gameplay';
                 initGameplay();
             }, 2000);
         }
